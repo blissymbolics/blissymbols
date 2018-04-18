0275cd6aa39adbcfe6deb95b4ab33367d7d7d6b7
-Lridge- - <hsls@hotmail.com>
2018-04-18 12:24:13 +0200

    use a more balanced default dot radius

--- blissviewer.js
+++ blissviewer.js
@@ -4,7 +4,7 @@ var BlissViewer = (function () {
         if (config === void 0) { config = {
             // Default config values
             margin: 8,
-            radius: 4
+            radius: 2
         }; }
         this.chardata = chardata;
         this.worddata = worddata;
